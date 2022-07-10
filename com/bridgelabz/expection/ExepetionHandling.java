bf93a5163d86682227d64d05150e3c41a0f305db
manishareddymuplam <manishamuplam@gmail.com>
2022-07-10 13:58:55 +0530

    added:LastName

--- com/bridgelabz/expection/ExepetionHandling.java
+++ com/bridgelabz/expection/ExepetionHandling.java
@@ -19,12 +19,27 @@ public class ExepetionHandling {
         else
             System.out.println("First Name is Invalid");
     }
+    public static void lastName() {
+
+        String lastNamePattern = "[A-Z]{1}[a-z]{2,}";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Last Name:");
+        String lastName = scanner.next();
+        Pattern pattern = Pattern.compile(lastNamePattern);
+        Matcher matcher = pattern.matcher(lastName);
+        boolean result = matcher.matches();
 
 
-
+        if(result == true)
+            System.out.println("Last Name is Valid");
+        else
+            System.out.println("Last Name is Invalid");
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Exception Handling");
         firstName();//UC1
+        lastName();//UC2
     }
 }
 
+    
